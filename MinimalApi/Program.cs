4e63bf95a72cd1281039b7cf86cc92f04032567d
Isiah Jones <jones.isiah.blhs@gmail.com>
2023-10-22 23:08:04 -0400

    simple error handling middleware

--- MinimalApi/Program.cs
+++ MinimalApi/Program.cs
@@ -6,6 +6,19 @@ builder.RegisterServices();
 
 var app = builder.Build();
 
+app.Use(async (ctx, next) =>
+{
+	try
+	{
+		await next();
+	}
+	catch (Exception)
+	{
+		ctx.Response.StatusCode = 500;
+		await ctx.Response.WriteAsync("An error occurred.");
+	}
+});
+
 app.UseHttpsRedirection();
 
 app.RegisterEndpointDefinitions();
