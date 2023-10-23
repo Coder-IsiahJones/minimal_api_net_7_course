20832f21db51c2b2e7fd12793a1df0ec2eddc618
Isiah Jones <jones.isiah.blhs@gmail.com>
2023-10-22 20:34:35 -0400

    Add migration

--- MinimalApi/Program.cs
+++ MinimalApi/Program.cs
@@ -1,4 +1,13 @@
+using Application.Abstractions;
+using DataAccess;
+using DataAccess.Repository;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<SocialDbContext>(options => options.UseSqlServer(connectionString));
+builder.Services.AddScoped<IPostRepository, PostRepository>();
 
 var app = builder.Build();
 
