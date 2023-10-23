971fd9824825e38955b5e8ba1fe6119886876699
Isiah Jones <jones.isiah.blhs@gmail.com>
2023-10-22 20:03:05 -0400

    Create Post

---
+++ Application/Abstractions/IPostRepository.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Models;
+
+namespace Application.Abstractions
+{
+    public interface IPostRepository
+    {
+        Task<ICollection<Post>> GetAllPostsAsync();
+
+        Task<Post> GetPostByIdAsync(int postId);
+
+        Task<Post> CreatePostAsync(Post post);
+
+        Task<Post> UpdatePostAsync(string updatedContent, int postId);
+
+        Task DeletePostAsync(int postId);
+    }
+}
---
+++ DataAccess/Repository/PostRepository.cs
@@ -0,0 +1,63 @@
+﻿using Application.Abstractions;
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Repository
+{
+    public class PostRepository : IPostRepository
+    {
+        private readonly SocialDbContext _context;
+
+        public PostRepository(SocialDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Post> CreatePostAsync(Post post)
+        {
+            post.DateCreated = DateTime.Now;
+            post.LastModified = DateTime.Now;
+
+            _context.Posts.Add(post);
+            await _context.SaveChangesAsync();
+
+            return post;
+        }
+
+        public async Task DeletePostAsync(int postId)
+        {
+            var post = await _context.Posts.FirstOrDefaultAsync(post => post.Id == postId);
+
+            if (post == null)
+            {
+                return;
+            }
+
+            _context.Posts.Remove(post);
+
+            await _context.SaveChangesAsync();
+        }   
+
+        public async Task<ICollection<Post>> GetAllPostsAsync()
+        {
+            return await _context.Posts.ToListAsync();
+        }
+
+        public async Task<Post> GetPostByIdAsync(int postId)
+        {
+            return await _context.Posts.FirstOrDefaultAsync(post => post.Id == postId);
+        }
+
+        public async Task<Post> UpdatePostAsync(string updatedContent, int postId)
+        {
+            var post = await _context.Posts.FirstOrDefaultAsync(post => post.Id == postId);
+
+            post.LastModified = DateTime.Now;
+            post.Content = updatedContent;
+
+            await _context.SaveChangesAsync();
+
+            return post;
+        }
+    }
+}
