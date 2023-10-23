9d4a7823fdc509ee904f79635bee6aea6a77cb57
Isiah Jones <jones.isiah.blhs@gmail.com>
2023-10-22 21:10:00 -0400

    Configure MediatR

---
+++ Application/Posts/CommandHandlers/DeletePostHandler.cs
@@ -0,0 +1,21 @@
+﻿using Application.Abstractions;
+using Application.Posts.Commands;
+using MediatR;
+
+namespace Application.Posts.CommandHandlers
+{
+    public class DeletePostHandler : IRequestHandler<DeletePost>
+    {
+        private readonly IPostRepository _postRepository;
+
+        public DeletePostHandler(IPostRepository postRepository)
+        {
+            _postRepository = postRepository;
+        }
+
+        public async Task Handle(DeletePost request, CancellationToken cancellationToken)
+        {
+            await _postRepository.DeletePostAsync(request.PostId);
+        }
+    }
+}
--- MinimalApi/Program.cs
+++ MinimalApi/Program.cs
@@ -1,6 +1,7 @@
 using Application.Abstractions;
 using DataAccess;
 using DataAccess.Repository;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<SocialDbContext>(options => options.UseSqlServer(connectionString));
 builder.Services.AddScoped<IPostRepository, PostRepository>();
+
+builder.Services.AddMediatR(typeof(Program));
 
 var app = builder.Build();
 
